d70b2e4c17f5516d9cb70b1e588c83d34ba457aa
Aaron <aaron.vanderpoel@gmail.com>
2015-11-03 18:07:48 -0500

    another reduce example

--- reduce/example.js
+++ reduce/example.js
@@ -1,3 +1,7 @@
+'use strict';
+
+// Simple example
+
 function sum() {
   var args = Array.prototype.slice.apply(arguments);
 
@@ -10,4 +14,28 @@ function sum() {
 
 console.log(
   sum(1, 2, 3, 4)
+);
+
+// More complex
+
+var data = [
+  ["aaron", "oldsmobile", "blue"],
+  ["aaron", "mazda", "silver"],
+  ["andrea", "subaru", "silver"],
+  ["mike", "toyota", "black"]
+];
+
+function formatData(data) {
+  return data.reduce((data, line) => {
+    data[line[0]] = data[line[0]] || [];
+    data[line[0]].push({
+      make: line[1],
+      color: line[2]
+    });
+    return data;
+  }, {});
+}
+
+console.log(
+  JSON.stringify(formatData(data), null, 2)
 );
